084b582bd066e51da214950b80fe163af2d96c4b
IbragimovAdel <ibragimovadel2001@gmail.com>
2018-04-29 09:02:41 +0300

    Меню

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,8 +3,11 @@ import './style.css';
 
 const Menu = () => {
     return(
-        <div>
-            Menu
+        <div className='menu'>
+            <div className='menu_item'><a href='#'>На главную</a></div>
+            <div className='menu_item'><a>ССЫЛКА#2</a></div>
+            <div className='menu_item'><a>ССЛЫКА#3</a></div>
+            <div className='searchIcon'><a>Иконка поиска</a></div>
         </div>
     )
 }
